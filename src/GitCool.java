80888b3e6fc982ac8d201ff7c00b3aa71b98f624
Jaffer4 <jaffer1ahmed@gmail.com>
2021-07-27 01:27:17 -0700

    added github account

--- src/GitCool.java
+++ src/GitCool.java
@@ -4,4 +4,10 @@ public class GitCool {
     // B23 IS MAKING SOME SENSE NOW
     // we have added our GitHub account under intellij
     // make sure to click apply and click on to save
+
+    // we were able to successfully shared project under our github account
+    // share project on github happen only one time per project
+    // it combines the 3 actions below
+    // Create the link between local repo to remote repo
+    // push local commits to remote repo
 }
