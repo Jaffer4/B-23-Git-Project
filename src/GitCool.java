7d6ac10a1b55ae94568770d00318c58aeb0ae26b
Jaffer4 <jaffer1ahmed@gmail.com>
2021-07-27 01:16:46 -0700

    added github account

--- src/GitCool.java
+++ src/GitCool.java
@@ -2,4 +2,6 @@ public class GitCool {
     // B23 IS LEARNING GIT
     // B23 IS A SPECIAL NUMBER
     // B23 IS MAKING SOME SENSE NOW
+    // we have added our GitHub account under intellij
+    // make sure to click apply and click on to save
 }
